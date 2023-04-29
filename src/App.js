054c4bff345c36f0ccbaf6c6c4f31e8d8eb3e92b
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-04-29 12:48:12 +0530

    modified-app-to-test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+<p>	Hello All</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
